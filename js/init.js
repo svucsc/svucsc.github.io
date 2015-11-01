da8fdb724e73912256ab6c4ec634d73f1cf211d0
Andrew King <mail@andrewcking.me>
2015-11-01 01:41:06 -0500

    semantic error

--- js/init.js
+++ js/init.js
@@ -3,7 +3,7 @@
 
        $('.button-collapse').sideNav();
        $(".dropdown-button").dropdown();
-       $( "p" ).removeClass( "fc-prev-button fc-button fc-state-default fc-corner-left" ).addClass( "waves-effect waves-light btn grey lighten-3" );
+       $( "button" ).removeClass( "fc-prev-button fc-button fc-state-default fc-corner-left" ).addClass( "waves-effect waves-light btn grey lighten-3" );
 
   }); // end of document ready
 })(jQuery); // end of jQuery name thing
