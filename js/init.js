707d143d4993d49479c1e2bc067d76475d1eb89e
Brandon Holland <Brandon592@gmail.com>
2015-11-10 12:50:49 -0500

    Added icons for subscribe links

--- js/init.js
+++ js/init.js
@@ -1,7 +1,7 @@
 (function($){
   $(function(){
 
-       $('.button-collapse').sideNav();
+       $('.button-collapse').sideNav({menuWidth: 300});
        $(".dropdown-button").dropdown();
        $( "button" ).removeClass( "fc-prev-button fc-button fc-state-default fc-corner-left" ).addClass( "waves-effect waves-light btn grey lighten-3" );
        $( "div.fc-right" ).addClass( "hide-on-med-and-down" );
