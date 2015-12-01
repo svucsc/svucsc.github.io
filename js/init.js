804c1f23d8f973c0501363294d1acc6567e7e47c
Andrew King <mail@andrewcking.me>
2015-12-01 08:43:42 -0500

    Show header buttons on mobile

--- js/init.js
+++ js/init.js
@@ -4,6 +4,5 @@
        $('.button-collapse').sideNav({menuWidth: 300});
        $(".dropdown-button").dropdown();
        $( "button" ).removeClass( "fc-prev-button fc-button fc-state-default fc-corner-left" ).addClass( "waves-effect waves-light btn grey lighten-3" );
-       $( "div.fc-right" ).addClass( "hide-on-med-and-down" );
   }); // end of document ready
 })(jQuery); // end of jQuery name thing
