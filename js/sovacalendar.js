b168123b4acdff128fd216d18cb650cc0590360d
Andrew King <mail@andrewcking.me>
2016-03-25 01:35:53 -0400

    Implement day view for mobile month

--- js/sovacalendar.js
+++ js/sovacalendar.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-	 
-    $('#calendar').fullCalendar({
+
+    $('#calendar, #calendar2').fullCalendar({
         //Calendar API key
         googleCalendarApiKey: 'AIzaSyDereJ50sioVk1EKNXt7m-dtNhEYweC_5A',
         //Allow the calendar to display 4,5 and 6 weeks depending on the month
@@ -39,7 +39,7 @@ $(document).ready(function() {
             month: {
                 eventLimit: 5
             },
-			basicDay: {
+            basicDay: {
                 eventLimit: 11
             },
             agendaThreeDay: {
@@ -50,180 +50,212 @@ $(document).ready(function() {
                 buttonText: '3 day'
             }
         },
+
         //Go to date functionality
+        //This also controls the second calendar view on month for mobile user
         dayClick: function(date, allDay, jsEvent, view) {
-            if (allDay) {
-                $('#calendar').fullCalendar('changeView', 'basicDay');
-                $('#calendar').fullCalendar('gotoDate', date);
+            if (window.innerWidth > 550) {
+                if (allDay) {
+                    $('#calendar').fullCalendar('changeView', 'basicDay');
+                    $('#calendar').fullCalendar('gotoDate', date);
+                }
+                getMonth();
+            } else {
+							//remove the highlights of the current day and add to selected day
+							//then display that date in calendar2
+							$(".fc-state-highlight").removeClass("fc-state-highlight");
+							$(".fc-today").removeClass("fc-today");
+							$("td[data-date="+date.format('YYYY-MM-DD')+"]").addClass("fc-state-highlight");
+                $('#calendar2').fullCalendar('gotoDate', date);
             }
-			getMonth();
+
         },
         //Event Modal Call
-        eventClick:  function(event, jsEvent, view) {
-          //field injections
+        eventClick: function(event, jsEvent, view) {
+            //field injections
 
             $('#modalTitle').html(event.title);
-			
-			if(event.description == undefined){
-				 $('#description').html("No Description Available");
-			} else {
-				$('#description').html(' ' + event.description);
-		}
-		
-		if(event.location == undefined){
-				 $('#location').html("");
-			} else {
-				$('#location').html(' ' + event.location);
-		}
-            
-			$('#colorId').html(event.colorId);
+
+            if (event.description == undefined) {
+                $('#description').html("No Description Available");
+            } else {
+                $('#description').html(' ' + event.description);
+            }
+
+            if (event.location == undefined) {
+                $('#location').html("");
+            } else {
+                $('#location').html(' ' + event.location);
+            }
+
+            $('#colorId').html(event.colorId);
             var startTime = new Date(event.start);
             var endTime = new Date(event.end);
-			var sTime = moment(startTime).format("h:mm a");
-			var eTime = moment(endTime).format("h:mm a");
-			if(sTime == eTime){
-				$('#time').html("All Day Event");
-				
-				
-			
-			 var eDate = moment(endTime).format("D, YYYY");
-			 
-			
-				var monthTest = moment(startTime).format("M");
-				var monthTest2 = monthTest-1;
-			if(monthTest == 3){
-				 sDate = moment(startTime).format("MMMM D");
-				 var new_day = moment(sDate, "DD-MM-YYYY").add(1, 'days');
-				 new_day = moment(new_day).format("MMMM D");
-				 var new_month = moment(new_day, "DD-MM-YYYY").add(monthTest2, 'months');
-				 new_month = moment(new_month).format("MMMM D");
-				 
-				 
-			} else if(monthTest == 4){
-				 sDate = moment(startTime).format("MMMM D");
-				 var new_day = moment(sDate, "DD-MM-YYYY").add(1, 'days');
-				 new_day = moment(new_day).format("MMMM D");
-				 var new_month = moment(new_day, "DD-MM-YYYY").add(monthTest2, 'months');
-				 new_month = moment(new_month).format("MMMM D");
-				
-			} else if(monthTest == 6){
-				 sDate = moment(startTime).format("MMMM D");
-				 var new_day = moment(sDate, "DD-MM-YYYY").add(1, 'days');
-				 new_day = moment(new_day).format("MMMM D");
-				 var new_month = moment(new_day, "DD-MM-YYYY").add(monthTest2, 'months');
-				 new_month = moment(new_month).format("MMMM D");
-				
-			} else if(monthTest == 7){
-				 sDate = moment(startTime).format("MMMM D");
-				 var new_day = moment(sDate, "DD-MM-YYYY").add(1, 'days');
-				 new_day = moment(new_day).format("MMMM D");
-				 var new_month = moment(new_day, "DD-MM-YYYY").add(monthTest2, 'months');
-				 new_month = moment(new_month).format("MMMM D");
-				
-			} else {
-				
-			 sDate = moment(startTime).format("MMM. D");
-				 var new_day = moment(sDate, "DD-MM-YYYY").add(1, 'days');
-				 new_day = moment(new_day).format("MMM. D");
-				 var new_month = moment(new_day, "DD-MM-YYYY").add(monthTest2, 'months');
-				 new_month = moment(new_month).format("MMM. D");
-			}
-			$('#date').html(new_month + ' - ' + eDate);
-				
-			} else {
-				$('#time').html(sTime + ' to ' + eTime);
-				var monthTest = moment(startTime).format("M");
-			if(monthTest == 3){
-				var sDate = moment(startTime).format("dddd, MMMM D, YYYY");
-			} else if(monthTest == 4){
-				var sDate = moment(startTime).format("dddd, MMMM D, YYYY");
-				
-			} else if(monthTest == 6){
-				var sDate = moment(startTime).format("dddd, MMMM D, YYYY");
-				
-			} else if(monthTest == 7){
-				var sDate = moment(startTime).format("dddd, MMMM D, YYYY");
-				
-			} else {
-				
-			var sDate = moment(startTime).format("dddd, MMM. D, YYYY");
-			}
-			$('#date').html(sDate);
-			}
-			
-            
+            var sTime = moment(startTime).format("h:mm a");
+            var eTime = moment(endTime).format("h:mm a");
+            if (sTime == eTime) {
+                $('#time').html("All Day Event");
+
+
+
+                var eDate = moment(endTime).format("D, YYYY");
+
+
+                var monthTest = moment(startTime).format("M");
+                var monthTest2 = monthTest - 1;
+                if (monthTest == 3) {
+                    sDate = moment(startTime).format("MMMM D");
+                    var new_day = moment(sDate, "DD-MM-YYYY").add(1, 'days');
+                    new_day = moment(new_day).format("MMMM D");
+                    var new_month = moment(new_day, "DD-MM-YYYY").add(monthTest2, 'months');
+                    new_month = moment(new_month).format("MMMM D");
+
+
+                } else if (monthTest == 4) {
+                    sDate = moment(startTime).format("MMMM D");
+                    var new_day = moment(sDate, "DD-MM-YYYY").add(1, 'days');
+                    new_day = moment(new_day).format("MMMM D");
+                    var new_month = moment(new_day, "DD-MM-YYYY").add(monthTest2, 'months');
+                    new_month = moment(new_month).format("MMMM D");
+
+                } else if (monthTest == 6) {
+                    sDate = moment(startTime).format("MMMM D");
+                    var new_day = moment(sDate, "DD-MM-YYYY").add(1, 'days');
+                    new_day = moment(new_day).format("MMMM D");
+                    var new_month = moment(new_day, "DD-MM-YYYY").add(monthTest2, 'months');
+                    new_month = moment(new_month).format("MMMM D");
+
+                } else if (monthTest == 7) {
+                    sDate = moment(startTime).format("MMMM D");
+                    var new_day = moment(sDate, "DD-MM-YYYY").add(1, 'days');
+                    new_day = moment(new_day).format("MMMM D");
+                    var new_month = moment(new_day, "DD-MM-YYYY").add(monthTest2, 'months');
+                    new_month = moment(new_month).format("MMMM D");
+
+                } else {
+
+                    sDate = moment(startTime).format("MMM. D");
+                    var new_day = moment(sDate, "DD-MM-YYYY").add(1, 'days');
+                    new_day = moment(new_day).format("MMM. D");
+                    var new_month = moment(new_day, "DD-MM-YYYY").add(monthTest2, 'months');
+                    new_month = moment(new_month).format("MMM. D");
+                }
+                $('#date').html(new_month + ' - ' + eDate);
+
+            } else {
+                $('#time').html(sTime + ' to ' + eTime);
+                var monthTest = moment(startTime).format("M");
+                if (monthTest == 3) {
+                    var sDate = moment(startTime).format("dddd, MMMM D, YYYY");
+                } else if (monthTest == 4) {
+                    var sDate = moment(startTime).format("dddd, MMMM D, YYYY");
+
+                } else if (monthTest == 6) {
+                    var sDate = moment(startTime).format("dddd, MMMM D, YYYY");
+
+                } else if (monthTest == 7) {
+                    var sDate = moment(startTime).format("dddd, MMMM D, YYYY");
+
+                } else {
+
+                    var sDate = moment(startTime).format("dddd, MMM. D, YYYY");
+                }
+                $('#date').html(sDate);
+            }
+
+
             //modal call per the materialize.css documentation
             $('#eventModal').openModal();
             //this prevents the eventclick redirect to google
             return false;
         }
-    //End fullcalendar brackets
+        //End fullcalendar brackets
     });
-    
-	//Switching the Banners
-		  function getMonth(){
-      var moment = $("#calendar").fullCalendar('getDate');
-      var month_int = moment.format('MM');
-  //you now have the visible month as an integer from 01-12
-	
-      if(month_int == '01'){
-		  $("#main-nav").removeClass();
-		  $("#main-nav").addClass("Jan");
-	  } else if (month_int == '02'){
-		  $("#main-nav").removeClass();
-		  $("#main-nav").addClass("Feb");
-	  } else if (month_int == '03'){
-		  $("#main-nav").removeClass();
-		  $("#main-nav").addClass("Mar");
-	  } else if (month_int == '04'){
-		  $("#main-nav").removeClass();
-		  $("#main-nav").addClass("Apr");
-	  } else if (month_int == '05'){
-		  $("#main-nav").removeClass();
-		  $("#main-nav").addClass("May");
-	  } else if (month_int == '06'){
-		  $("#main-nav").removeClass();
-		  $("#main-nav").addClass("Jun");
-	  } else if (month_int == '07'){
-		  $("#main-nav").removeClass();
-		  $("#main-nav").addClass("Jul");
-	  } else if (month_int == '08'){
-		  $("#main-nav").removeClass();
-		  $("#main-nav").addClass("Aug");
-	  } else if (month_int == '09'){
-		  $("#main-nav").removeClass();
-		  $("#main-nav").addClass("Sept");
-	  } else if (month_int == '10'){
-		  $("#main-nav").removeClass();
-		  $("#main-nav").addClass("Oct");
-	  } else if (month_int == '11'){
-		  $("#main-nav").removeClass();
-		  $("#main-nav").addClass("Nov");
-	  } else {
-		  $("#main-nav").removeClass();
-		  $("#main-nav").addClass("Dec");
-	  } 
-	
-}
-	
-	getMonth();
-	
-	$('.fc-right').click(function() {
-    getMonth();
-});
 
-	 // Back Button
-	    var view = $('#calendar').fullCalendar('getView');
-        var back = view.name;	 
-$("#prev").click(function() {
+
+		//This section deals with the day view on month view for mobile users
+    //change the mobile second calendar to day view for use with month view
+    $('#calendar2').fullCalendar('changeView', 'basicDay');
+    function setMobileCal() {
+        var view = $('#calendar').fullCalendar('getView');
+        if (view.name == 'month' && window.innerWidth < 551) {
+            $("#calendar2").css({
+                "display": "block"
+            });
+        } else {
+            $("#calendar2").css({
+                "display": "none"
+            });
+        }
+    }
+    //if we resize the window we need to call setmobilecal
+    $(window).resize(function() {
+        setMobileCal();
+    });
+
+
+
+    //Switching the Banners
+    function getMonth() {
+        var moment = $("#calendar").fullCalendar('getDate');
+        var month_int = moment.format('MM');
+        //you now have the visible month as an integer from 01-12
+
+        if (month_int == '01') {
+            $("#main-nav").removeClass();
+            $("#main-nav").addClass("Jan");
+        } else if (month_int == '02') {
+            $("#main-nav").removeClass();
+            $("#main-nav").addClass("Feb");
+        } else if (month_int == '03') {
+            $("#main-nav").removeClass();
+            $("#main-nav").addClass("Mar");
+        } else if (month_int == '04') {
+            $("#main-nav").removeClass();
+            $("#main-nav").addClass("Apr");
+        } else if (month_int == '05') {
+            $("#main-nav").removeClass();
+            $("#main-nav").addClass("May");
+        } else if (month_int == '06') {
+            $("#main-nav").removeClass();
+            $("#main-nav").addClass("Jun");
+        } else if (month_int == '07') {
+            $("#main-nav").removeClass();
+            $("#main-nav").addClass("Jul");
+        } else if (month_int == '08') {
+            $("#main-nav").removeClass();
+            $("#main-nav").addClass("Aug");
+        } else if (month_int == '09') {
+            $("#main-nav").removeClass();
+            $("#main-nav").addClass("Sept");
+        } else if (month_int == '10') {
+            $("#main-nav").removeClass();
+            $("#main-nav").addClass("Oct");
+        } else if (month_int == '11') {
+            $("#main-nav").removeClass();
+            $("#main-nav").addClass("Nov");
+        } else {
+            $("#main-nav").removeClass();
+            $("#main-nav").addClass("Dec");
+        }
+        //this calls the method to update the mobile view for the second day calendar on month view
+        setMobileCal();
+    }
+
+    getMonth();
+
+    $('.fc-right').click(function() {
+        getMonth();
+    });
+
+    // Back Button
+    var view = $('#calendar').fullCalendar('getView');
+    var back = view.name;
+    $("#prev").click(function() {
         $('#calendar').fullCalendar('changeView', back);
         $('.button-collapse').sideNav('hide');
-		getMonth();
+        getMonth();
     });
-	
-	
-
-
 
 
 
@@ -239,7 +271,7 @@ $("#prev").click(function() {
             $('#calendar').fullCalendar('removeEventSource', 'svu.edu_nf5v7viog32c563doboaljtf4o@group.calendar.google.com')
         }
     })
-	$('#clubs').click(function() {
+    $('#clubs').click(function() {
         if (this.checked) {
             $('#calendar').fullCalendar('addEventSource', {
                 url: "svu.edu_tsbvq83kmqb2qe1qnocjd7v5f4@group.calendar.google.com",
@@ -290,47 +322,43 @@ $("#prev").click(function() {
         }
     })
     $('#holiday').click(function() {
-        if (this.checked) {
-            $('#calendar').fullCalendar('addEventSource', {
-                url: "usa__en@holiday.calendar.google.com",
-                color: '#607D8B'
-            });
-        } else {
-            $('#calendar').fullCalendar('removeEventSource', 'usa__en@holiday.calendar.google.com')
-        }
-    })
-    //View buttons for sidenav
+            if (this.checked) {
+                $('#calendar').fullCalendar('addEventSource', {
+                    url: "usa__en@holiday.calendar.google.com",
+                    color: '#607D8B'
+                });
+            } else {
+                $('#calendar').fullCalendar('removeEventSource', 'usa__en@holiday.calendar.google.com')
+            }
+        })
+        //View buttons for sidenav
     $("#day").click(function() {
-		view = $('#calendar').fullCalendar('getView');
+        view = $('#calendar').fullCalendar('getView');
         back = view.name;
         $('#calendar').fullCalendar('changeView', 'basicDay');
         $('.button-collapse').sideNav('hide');
-		getMonth();
+        getMonth();
     });
     $("#threeday").click(function() {
-		view = $('#calendar').fullCalendar('getView');
+        view = $('#calendar').fullCalendar('getView');
         back = view.name;
         $('#calendar').fullCalendar('changeView', 'agendaThreeDay');
         $('.button-collapse').sideNav('hide');
-		getMonth();
+        getMonth();
     });
     $("#week").click(function() {
-		view = $('#calendar').fullCalendar('getView');
+        view = $('#calendar').fullCalendar('getView');
         back = view.name;
         $('#calendar').fullCalendar('changeView', 'agendaWeek');
         $('.button-collapse').sideNav('hide');
-		getMonth();
+        getMonth();
     });
     $("#month").click(function() {
-		view = $('#calendar').fullCalendar('getView');
+        view = $('#calendar').fullCalendar('getView');
         back = view.name;
         $('#calendar').fullCalendar('changeView', 'month');
         $('.button-collapse').sideNav('hide');
-		getMonth();
+        getMonth();
     });
-      if ($(window).width() < 650) {
-        $('#calendar').fullCalendar('changeView', 'basicDay');
-    } else {
-        $('#calendar').fullCalendar('changeView', 'month');
-    }
+
 });
